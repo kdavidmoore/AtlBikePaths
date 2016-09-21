6038348809892d42cf9c64d02ec450f416f882f7
Keith Moore <keith.m4@gmail.com>
2016-09-20 21:03:07 -0400

    removed extraneous plus operator

--- assets/js/listeners.js
+++ assets/js/listeners.js
@@ -20,13 +20,13 @@ $(document).ready(function(){
     $.get(featureInfoUrl, function(response) {
 
       if(response.features[0]) {
-        var surfType = String(response.features[0].properties.ogr_surf_s) || '';
-        var featM = String(response.features[0].properties.ogr_feat_m) || '';
-        var offRoad = String(response.features[0].properties.ogr_on_off) || '';
-        var facType = String(response.features[0].properties.ogr_factyp) ||
-          String(response.features[0].ogr_fact_1);
-        var pathLoc = String(response.features[0].properties.ogr_region) ||
-          String(response.features[0].properties.ogr_regi_1);
+        var surfType = response.features[0].properties.ogr_surf_s || '';
+        var featM = response.features[0].properties.ogr_feat_m || '';
+        var offRoad = response.features[0].properties.ogr_on_off || '';
+        var facType = response.features[0].properties.ogr_factyp ||
+          response.features[0].ogr_fact_1;
+        var pathLoc = response.features[0].properties.ogr_region ||
+          response.features[0].properties.ogr_regi_1;
       } else {
         var surfType = 'unknown';
         var featM = 'unknown';
@@ -39,9 +39,10 @@ $(document).ready(function(){
         $('#modal1').openModal();
         var modalHeight = $('#modal1').height();
         $('.modal-header').text(facType);
-        $('.modal-table-body').html('<tr><td>' + pathLoc + '</td><td>' +
-          + surfType + '</td><td>' + offRoad + '</td><td>' + featM +
-          '</td></tr>');
+        var modalTable = '<td>' + pathLoc + '</td><td>' +
+          surfType + '</td><td>' + offRoad + '</td><td>' +
+          featM + '</td>';
+        $('.modal-table-body').html(modalTable);
         var bikeImg = '<img class="modal-img" src="exifreader/images/' +
           feature.get('imgSrc') + '">';
         $('.modal-img-wrapper').html(bikeImg);
